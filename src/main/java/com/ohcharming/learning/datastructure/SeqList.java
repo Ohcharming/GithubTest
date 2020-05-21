8010903255e7aa98ac34f56fb6fc484901cc35f9
崔艳伟 <120182154@qq.com>
2020-05-21 15:16:33 +0800

    调整代码位置

--- src/main/java/com/ohcharming/learning/datastructure/SeqList.java
+++ src/main/java/com/ohcharming/learning/datastructure/SeqList.java
@@ -1,62 +1,102 @@
 package com.ohcharming.learning.datastructure;
 
-/**
- * 〈数序表的一种实现-数据结构〉
- *
- * @author cuiyanwei
- * @create 2020/5/18
- * @since 1.0.0
- */
-public class SeqList {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+public class SeqList{
+	
+   
+    private int[] data;//数组存储数据
+    private int size;//数据占用空间的大小
 
-    }
-    public static final int MAXSIZE = 20;//20个存储空间
-    private T[] data;//数组存储数据
-    private int Length;//顺序表的长度
-
-    //插入元素
-    public boolean insertElem(int i,T t){//i表示要插入的位置,t表示待插入的元素
-        if (Length==MAXSIZE) {
-            System.out.println("线性表已经满");
-            return false;
-        }
-        if (i<1 || i>Length+1) {
-            return false;
-        }
-        if (i<=Length) {
-            for(int j=Length-1;j>=i-1;j--){
-                data[j+1] = data[j];//前面的值赋值到后面
-            }
-        }
-
-        data[i-1] = t;//
-        Length++;
-        return true;
-
+    //初始化线性表声明数组的大小
+    public SeqList(int initial_size) {
+    	if(initial_size>=0) {
+    		this.data=new int[initial_size];//初始化数组
+    		this.size=0;//初始元素第几位
+    	}else
+    		System.out.println("初始值不能小于0");
     }
 
-    //删除元素
-    public boolean deleteElem(int i){
-        if(Length==0){
-            System.out.println("此线性表为空");
-            return false;
-        }
-
-        if (i<1 || i>Length) {
-            System.out.println("删除的位置不在范围内");
-            return false;
-        }
-
-        @SuppressWarnings("unused")
-        T t = data[i-1];
-        if (i<Length) {
-            for (int j = i-1; j < Length; j++) {
-                data[j] = data[j+1];
-            }
-            return true;
-        }
-
-    }
+     //判断线性表是否为空
+      public boolean isEmpty() {
+      if(this.size==0) {
+    	  return true;
+     }else {
+    	 return false;
+     }
 }
+      //判断线性表是否为满
+      public boolean isFull() {
+    	  if(this.size==data.length) {
+    		  return true;
+    	  }else {
+    		  return false;
+    	  }
+      }
+      //增加元素
+      public void addElem(int value,int index) {
+    	  if(isFull()) {
+    		  System.out.println("此顺序表已满");
+    	  }else if(index<0||index>data.length) {
+    		  System.out.println("应该插入正的下标");
+    	  }else {
+    		  for(int i=this.size-1;i>=index;i--) {
+    			data[i+1]=data[i];//依次向后移  
+    		  }
+    		  data[index]=value;
+    		  this.size++;//元素下标增加
+    	  }
+      }
+      //删除元素
+      public void deleteElem(int value) {
+          int pos =value;
+          if (pos == -1) {
+              System.out.println( + value + " 元素不在该表中");
+          } else {
+              System.out.println( + value + " 元素下标为：" + pos);
+          }
+
+          for (int j = pos; j <= this.size - 2; j++) {
+              data[j] = data[j + 1];
+          }
+          this.size--;
+      }
+      //查找元素在第几位
+      public int find(int value) {
+    	  int pos=-1;
+    	  if(isEmpty()) {
+    		  System.out.println("线性表是空的没有元素可以删除");
+    	  }else {
+    		  for(int i=0;i<=this.size-1;i++) {
+    			  if(data[i]==value) {
+    				  pos=i;
+    			  }
+    		  }
+    	  }
+    	  return pos;
+      }
+      
+      public void print() {
+    	  for(int i=0;i<=this.size-1;i++) {
+    		  System.out.println(data[i]+" ");
+    	  }
+    	  System.out.println();
+      }
+      public static void main(String[]args) {
+    	  SeqList SeqList=new SeqList(10);
+    	  for(int i=0;i<10;i++) {
+    		  SeqList.addElem(i+1,i);
+    	  }
+    	  SeqList.print();
+    	  //删除元素
+          SeqList.deleteElem(5);
+          SeqList.print();
+    	 
+         //查找表内的元素
+         int find_value=2;
+	     int pos=SeqList.find(find_value);
+	     if(pos==-1) {
+		  System.out.println(+ find_value + " 元素不在该表中");
+	  }else
+		  System.out.println( + find_value + " 元素下标为：" + pos);
+  }
+    	 
+      }
--- src/main/java/com/ohcharming/learning/datastructure/SingleLinkedList.java
+++ src/main/java/com/ohcharming/learning/datastructure/SingleLinkedList.java
@@ -1,111 +1,165 @@
 package com.ohcharming.learning.datastructure;
 
-/**
- * 〈单链表的一种实现-数据结构〉
- *
- * @author cuiyanwei
- * @create 2020/5/18
- * @since 1.0.0
- */
-public class SingleLinkedList {
-    public static void main(String[] args) {
-    }
-    // TODO Auto-generated method stub
-    public  AnyType data;
-    public  MyLinkedList<AnyType> next;
-    public MyLinkedList(AnyType data,MyLinkedList<AnyType> next){
-        this.data=data;
-        this.next=next;
-    }
-
+public class SingleLinkedList <T>{
+	
+	private class Node{
+		private T t;
+		private Node next;
+		public Node(T t,Node next){
+			this.t = t;
+			this.next = next;
+		}
+		public Node(T t){
+			this(t,null);
+		}
+	}
+	private Node head;    		//头结点
+	private int size;			//链表元素个数
+	
+	public SingleLinkedList(){
+		this.head = null;
+		this.size = 0;
+	}
+	
+	//获取链表元素的个数
+	public int getSize(){
+		return this.size;
+	}
+	
+	
+	//链表头部添加元素
+	public void addFirst(T t){
+		Node node = new Node(t);	//节点对象
+		node.next = this.head;
+		this.head = node;
+		this.size++;
+		
+	}
+	//向链表尾部插入元素
+	public void addLast(T t){
+		this.add(t, this.size);
+	}
+	//向链表中间插入元素
+	public void add(T t,int index){
+		if (index <0 || index >size){
+			throw new IllegalArgumentException("index is error");
+		}
+		if (index == 0){
+			this.addFirst(t);
+		}
+		Node preNode = this.head;
+		//找到要插入节点的前一个节点
+		for(int i = 0; i < index-1; i++){
+			preNode = preNode.next;
+		}
+		Node node = new Node(t);
+		//要插入的节点的下一个节点指向preNode节点的下一个节点
+		node.next = preNode.next;
+		//preNode的下一个节点指向要插入节点node
+		preNode.next = node;
+		this.size++;
+	}
+	//删除链表元素
+	public void remove(T t){
+		if(head == null){
+			System.out.println("无元素可删除");
+			return;
+		}
+		//要删除的元素与头结点的元素相同
+		while(head != null && head.t.equals(t)){
+			head = head.next;
+			this.size--;
+		}
+				
+		 //要对头结点的下一个结点进行判别
+		
+		Node cur = this.head;
+		while(cur.next != null){
+			if(cur.next.t.equals(t)){
+				this.size--;
+				cur.next = cur.next.next;
+			}
+			else cur = cur.next;
+		}
+		
+	}
+	//删除链表第一个元素
+	public T removeFirst(){
+		if(this.head == null){
+			System.out.println("无元素可删除");
+			return null;
+		}
+		Node delNode = this.head;
+		this.head = this.head.next;
+		delNode.next =null;
+		this.size--;
+		return delNode.t;
+	}
+	//删除链表的最后一个元素
+	public T removeLast(){
+		if(this.head == null){
+			System.out.println("无元素可删除");
+			return null;
+		}
+		//只有一个元素
+		if(this.getSize() == 1){
+			return this.removeFirst();
+		}
+		Node cur = this.head;	//记录当前结点
+		Node pre = this.head;	//记录要删除结点的前一个结点
+		while(cur.next != null){
+			pre = cur;
+			cur = cur.next;
+		}
+		pre.next = cur.next;
+		this.size--;
+		return cur.t;
+	}
+	//链表中是否包含某个元素
+	public boolean contains(T t){
+		Node cur = this.head;
+		while(cur != null){
+			if(cur.t.equals(t)){
+				return true;
+			}
+			else cur = cur.next;
+		}
+		return false;
+	}
+	//1、可以表示下面的方法是重写的父类的方法
+	//2、有注释的作用，加上这个标记就说明是重写的父类的方法，调试程序比较方便。
+	//3、在方法的前面加上@override系统就可以帮助你检查方法的正确性。
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		Node cur = this.head;
+		while(cur != null){
+			sb.append(cur.t+"->");
+			cur = cur.next;
+		}
+		sb.append("NULL");
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		SingleLinkedList<Integer> linked = new SingleLinkedList();
+		for(int i = 1; i < 20; i++){
+			linked.addFirst(i);
+			System.out.println(linked);
+		}
+		//都是在分号的基础上继续下一个操作
+		linked.addLast(33);
+		
+		linked.add(33, 10);
+		System.out.println(linked);
+		linked.remove(2);
+		linked.remove(3);
+		System.out.println(linked);
+		System.out.println("删除第一个元素："+linked.removeFirst());
+		System.out.println(linked);
+		System.out.println("删除最后一个元素："+linked.removeLast());
+		System.out.println(linked);
+	}
 }
 
-class MyLinkedList<AnyType> {
-
-    //首元节点
-    private MyLinkedList<AnyType> first;
-
-    //头指针
-    private MyLinkedList<AnyType> head;
-
-    //链表长度
-    int thesize;
-
-    //初始化链表
-    public boolean initlist() {
-        thesize = 0;
-        first = new MyLinkedList<>(null, null);
-        head = new MyLinkedList<>(null, first);
-        return true;
-    }
-
-    //判断链表是否为空
-    public boolean isEmpty() {
-        return thesize == 0;
-    }
-
-    //获取节点
-    public MyLinkedList<AnyType> getMyLinkedList(int i) {
-        MyLinkedList<AnyType> renode = head;
-        for (int j = -2; j < i; j++) {
-            renode = renode.next;
-        }
-        return renode;
-    }
-
-    //在末尾添加元素
-    public void add(AnyType a) {
-        MyLinkedList<AnyType> renode = new MyLinkedList<>(a, null);
-        getMyLinkedList(thesize - 1).next = renode;
-        thesize++;
-    }
-
-    //删除i位置节点，并返回删掉的数据
-    public AnyType remove(int i) {
-        if (i == thesize - 1) {
-            AnyType a = getMyLinkedList(thesize - 1).data;
-            getMyLinkedList(thesize - 2).next = null;
-            return a;
-        }
-        MyLinkedList<AnyType> prev = getMyLinkedList(i - 1);
-        AnyType a = prev.next.data;
-        prev.next = prev.next.next;
-        thesize--;
-        return a;
-    }
-
-    //在i位置插入新节点
-    public void insert(int i, AnyType a) {
-        MyLinkedList<AnyType> prev = getMyLinkedList(i - 1);
-        MyLinkedList<AnyType> renode = new MyLinkedList<>(a, prev.next);
-        prev.next = renode;
-        thesize++;
-    }
-
-    //获取i位置节点的数据
-    public AnyType get(int i) {
-        return getMyLinkedList(i).data;
-    }
-
-    //为i位置元素重新赋值
-    public void set(int i, AnyType a) {
-        getMyLinkedList(i).data = a;
-    }
-
-    //返回链表节点个数
-    public int length() {
-        return thesize;
-    }
-
-    //清空链表
-    public void clear() {
-        initlist();
-    }
-
-    //打印链表
-    public void print() {
-        for (int i = 0; i < thesize; i++) {
-            System.out.println(getMyLinkedList(i).data);
-        }
-    }
-}
+	
